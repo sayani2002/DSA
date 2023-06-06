a66838f997b244cf321e712ff9c021a89d786a96
sayani2002 <121875526+sayani2002@users.noreply.github.com>
2023-06-06 13:54:54 +0530

    commit

--- ARRAYS/1.1 Reverse a string.py
+++ ARRAYS/1.1 Reverse a string.py
@@ -1,5 +1,11 @@
+a= input()
+def reverse_string(input_string):
+    reversed_string = ''
+    for char in input_string:
+        reversed_string = char + reversed_string
+    return reversed_string
 
-
+print (reverse_string(a))
 
 
 
